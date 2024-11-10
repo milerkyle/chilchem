f6e4858cd7cb4c6cf2442fc416072bb08d35b473
Kyle Miler <grinning2024@outlook.com>
2024-11-10 12:30:51 +0800

    test again

--- chilchem/functions/test.js
+++ chilchem/functions/test.js
@@ -1,14 +1,15 @@
 import book from "../books.html";
 export default{
   async fetch(request,env,ctx){
-    const url = new URL(request.url);
+    /**
+     * const url = new URL(request.url);
     console.log(url.pathname);
     console.log(url);
     if (url.pathname === ""){
       const data = " I am a response!";
       return Response.json(data);
     }
-    
+    **/
     return new Response(book,{
       headers:{
         "content-type":"text/html",
