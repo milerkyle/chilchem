6721fc80725ad2c1e0b5fa60d4552d1bed8bea72
Kyle Miler <grinning2024@outlook.com>
2024-11-16 22:13:57 +0800

    Update helloworld.js

--- chilchem/functions/helloworld.js
+++ chilchem/functions/helloworld.js
@@ -1,5 +1,9 @@
 import html from "../books.html"
 export function onRequest(context) {
-    return new Response(html)
+    return new Response(html,{
+        headers:{
+            "content-type":"text-html"
+        }
+    })
   }
   
