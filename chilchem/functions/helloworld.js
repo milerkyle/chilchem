ad90706f61f6dd5520b5381da40db96ed3236db9
Kyle Miler <grinning2024@outlook.com>
2024-11-17 12:50:16 +0800

    set charset

--- chilchem/functions/helloworld.js
+++ chilchem/functions/helloworld.js
@@ -2,7 +2,7 @@ import html from "../books.html"
 export function onRequest(context) {
     return new Response(html,{
         headers:{
-            "content-type":"text-html;charset = utf-8"
+            "content-type":"text-html;charset = zh-CN"
         }
     })
   }
