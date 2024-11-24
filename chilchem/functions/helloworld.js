a6297f20171e63e6ee2fd160af05b4a4733b2021
Kyle Miler <grinning2024@outlook.com>
2024-11-24 13:04:31 +0800

    trial1

--- chilchem/functions/helloworld.js
+++ chilchem/functions/helloworld.js
@@ -2,7 +2,7 @@ import html from "../books.html"
 export function onRequest(context) {
     let db = context.env.chilchem-db;
     let stmt = db.prepare("SELECT * FROM books WHERE name=?");
-    let result = await stmt.all();
+    let result = stmt.all();
     return new Response(result,{
         headers:{
             "content-type":"text"
