31e22186934959b85cbdf3bb4de27e566d217ac6
Kyle Miler <grinning2024@outlook.com>
2024-11-24 13:19:50 +0800

    trial5

--- chilchem/functions/helloworld.js
+++ chilchem/functions/helloworld.js
@@ -1,12 +1,10 @@
 import html from "../books.html"
 export async function onRequest(context) {
     let db = context.env.chilchem-db;
-    let stmt = db.prepare("SELECT * FROM books");
+    let stmt = db.prepare("SELECT * FROM books WHERE name=?").bind("化学中的多面体");
     let result = await stmt.all();
     return new Response(result,{
-        headers:{
-            "content-type":"text"
-        }
+        status:200,
     })
   }
   
