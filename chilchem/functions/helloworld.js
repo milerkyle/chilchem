84fd3725a48b2d755e40a81a979abc243f3b3db8
Kyle Miler <grinning2024@outlook.com>
2024-12-01 13:10:13 +0800

    trial23

--- chilchem/functions/helloworld.js
+++ chilchem/functions/helloworld.js
@@ -1,18 +1,18 @@
 import html from "../books.html"
-export async fetch(request,context){
-        let result
-        try {
-            const stmt =context.env.chilchem.prepare("SELECT * FROM books");
-            let {result} = await stmt.all();
-            /*result = db*/
-        } catch(error){
-            result = error;
-        }
-    }
-    
-    
-    return new Response(result,{
-        status:200,
-    })
-  }
-  
+export function(){
+        async fetch(request,env){
+                let result
+                try {
+                    const stmt =context.DATABASE.prepare("SELECT * FROM books");
+                    let {result} = await stmt.all();
+                    /*result = db*/
+                } catch(error){
+                    result = error;
+                }
+            }
+            
+            
+            return new Response(result,{
+                status:200,
+            })
+}
