312730c27f25586edfd6f82ca9125f614778509c
Kyle Miler <grinning2024@outlook.com>
2024-12-01 13:22:23 +0800

    trial28

--- chilchem/functions/helloworld.js
+++ chilchem/functions/helloworld.js
@@ -2,7 +2,7 @@ import html from "../books.html"
 export async function onRequest(context,env,ctx) {
     let result
     try {
-        const db = env.DATABASE;
+        const db = context.DATABASE;
         const stmt = db.prepare("SELECT * FROM books");
         let {result} = await stmt.all();
         /*result = db*/
