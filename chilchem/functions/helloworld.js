7b6eeee67bb594c1f37dbd5b46bbeebcbae04afe
Kyle Miler <grinning2024@outlook.com>
2024-12-01 11:32:08 +0800

    trial9

--- chilchem/functions/helloworld.js
+++ chilchem/functions/helloworld.js
@@ -1,11 +1,16 @@
 import html from "../books.html"
 export async function onRequest(context) {
-    const db = context.env.chilchem-db;
-    /*const stmt = db.prepare("SELECT * FROM books WHERE name=?").bind("化学中的多面体");
-    const result = await stmt.all();*/
-    let result = "success?"
+    let result
+    try {
+        const db = context.env.chilchem-db;
+        const stmt = db.prepare("SELECT * FROM books WHERE name=?").bind("化学中的多面体");
+        result = await stmt.all();
+    } catch(error){
+        result = error.name
+    }
+    
     return new Response(result,{
-        /*status:200,*/
+        status:200,
     })
   }
   
