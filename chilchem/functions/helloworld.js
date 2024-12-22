8d6496c0c8db93a21054403228963d2689fedd61
Kyle Miler <grinning2024@outlook.com>
2024-12-22 12:24:14 +0800

    ffix

--- chilchem/functions/helloworld.js
+++ chilchem/functions/helloworld.js
@@ -1,16 +1,14 @@
 import html from "../books.html"
 export async function onRequest(context) {
-    async fetch(request,env,ctx){
-        const url = new URL(request.url);
-        const content = String(url.search);
+
         const stmt = context.env.DATABASE.prepare("SELECT * FROM books WHERE name =?").bind("");
         const results = await stmt.raw();
 
-        /*return Response.json(results);
+        /*return Response.json(results);*/
 
-        return new Response(url,{
+        return new Response(results,{
             status:200,
-        })*/
+        })
     },
 
     
