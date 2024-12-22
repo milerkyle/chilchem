e20eb5fe8dce0c8f9eae829e046b6898b41d9962
Kyle Miler <grinning2024@outlook.com>
2024-12-22 12:20:17 +0800

    fix

--- chilchem/functions/testa.js
+++ chilchem/functions/testa.js
@@ -1,21 +1,22 @@
 import html from "../test-papers.html";
 export default{
   async fetch(request,env,ctx){
-    /**
-     * const url = new URL(request.url);
-    console.log(url.pathname);
-    console.log(url);
-    if (url.pathname === ""){
+    
+    const url = new URL(request.url);
+    const content = url.search;
+    return Response.json(url);
+    /*if (url.pathname === ""){
       const data = " I am a response!";
       return Response.json(data);
     }
-    **/
+  
     return new Response(html,{
       status: 200 ,
       headers:{
         "content-type":"text/html",
       },
     });
+    */
     
   },
 };
