243d639dd67add19bbf7e2ee5b2bf78daca6fcf3
Kyle Miler <grinning2024@outlook.com>
2024-12-22 11:56:15 +0800

    try8

--- chilchem/functions/helloworld.js
+++ chilchem/functions/helloworld.js
@@ -1,7 +1,7 @@
 import html from "../books.html"
 export async function onRequest(context) {
     const url = new URL(request.url);
-    const content = url.search;
+    const content = String(url.search);
     const stmt = context.env.DATABASE.prepare("SELECT * FROM books");
     const results = await stmt.raw();
 
