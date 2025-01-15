07d4fc7de19478e3ddf6db44fa9b9d8d0359a5b0
Kyle Miler <grinning2024@outlook.com>
2025-01-15 20:31:49 +0800

    doubt

--- chilchem/functions/helloworld.js
+++ chilchem/functions/helloworld.js
@@ -1,12 +1,15 @@
 import html from "../books.html"
-export async function onRequest(context) {
+export function onRequest(context) {
+  return new Response("Hello, world!")
+}
+/*export async function onRequest(context) {
 
         const stmt = context.env.DATABASE.prepare("SELECT * FROM books");
         const results = await stmt.raw();
         const result = String(results);
-/*
+
         return Response.json(results);
-        */
+        
         const res = JSON(results);
 
         return new Response("helloworld",{
@@ -15,9 +18,10 @@ export async function onRequest(context) {
                         "content-type":"text",
                         /*"Access-Control-Allow-Origin":"*",
                         
-                        "X-Content-Type-Options": "no-sniff",*/
+                        "X-Content-Type-Options": "no-sniff",
                 }
         })
 
     
   }
+*/
